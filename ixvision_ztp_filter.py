a68e16ae76101447361542442f1076ce92b447e0
Alex Bortok <dev@bortok.com>
2018-08-14 09:10:59 -0700

    DF criteria can be supplied
    -c <filter_criteria_file> in JSON format

--- ixvision_ztp_filter.py
+++ ixvision_ztp_filter.py
@@ -24,6 +24,10 @@ df_modes_supported = {'all': 'PASS_ALL', 'none': 'DISABLE', 'pbc': 'PASS_BY_CRIT
 
 # DEFINE FUNCTIONS HERE
 
+# Check if DF mode used requires a criteria
+def df_criteria_required(df_mode):
+    return df_mode in df_modes_supported and (df_mode == 'pbc' or df_mode == 'dbc')
+
 ## Search for port groups and return a list if IDs
 def search_port_group_id_list(nto, params):
     pg_id_list = []
@@ -42,16 +46,17 @@ def search_port_group_id_list(nto, params):
 # - Tool port group name
 # - DF mode - use keys from df_modes_supported global dict
 
-def form_dynamic_filter(host_ip, port, username, password, df_name, input_pg_name, output_pg_name, df_mode):
+def form_dynamic_filter(host_ip, port, username, password, df_name, input_pg_name, output_pg_name, df_mode, df_criteria = {}):
     
     df_params = {}
     df_mode_value = 'DISABLE'                           # Default DF mode value for a new filter to use, if not overridden
-    df_criteria_value = {                               # TODO TEMPORARY - Replace with REQUIRED CRITERIA Default DF criteria for PBC, DBC modes for a new filter to use, if not overridden
-        "ethertype": {"value": "800"},
-        "logical_operation": "AND"
-    }
+
     if isinstance(df_modes_supported, dict) and df_mode in df_modes_supported.keys():
         df_mode_value = df_modes_supported[df_mode]
+        
+    if df_criteria_required(df_mode) and (isinstance(df_criteria, dict) and len(df_criteria) == 0 or not isinstance(df_criteria, dict)):
+        print("Non-empty criteria are required for filter mode %s" % (df_mode))
+        return
                 
     nto = NtoApiClient(host=host_ip, username=username, password=password, port=port, debug=True, logFile="ixvision_ztp_filter_debug.log")
 
@@ -63,8 +68,8 @@ def form_dynamic_filter(host_ip, port, username, password, df_name, input_pg_nam
     if len(df_list) == 0:
         # No existing filter with such name, will create a new one
         df_params.update({'name': df_name, 'keywords': ['_ZTP_LLDP'], 'mode': df_mode_value})
-        if df_mode_value == df_modes_supported['pbc'] or df_mode_value == df_modes_supported['dbc']:
-            df_params.update({'criteria': df_criteria_value})
+        if isinstance(df_criteria, dict) and len(df_criteria) > 0:
+            df_params.update({'criteria': df_criteria})
         new_df = nto.createFilter(df_params, True) # the last parameter is for allowTemporayDataLoss
         if new_df is not None and len(new_df) > 0:
             print("No existing DF found, created a new one with id %s" % (str(new_df['id'])))
@@ -80,15 +85,21 @@ def form_dynamic_filter(host_ip, port, username, password, df_name, input_pg_nam
         ztp_df_source_port_group_id_list = df_details['source_port_group_list']
         ztp_df_dest_port_group_id_list = df_details['dest_port_group_list']
         print("Found an existing DF %s in %s mode" % (df_details['default_name'], df_details['mode']))
+        df_needs_updating = False
         # TODO update keywords with _ZTP_LLDP
         if df_mode_value != df_details['mode']:
             print("Updating DF %s to a new mode %s" % (df_details['default_name'], df_mode_value))
             df_params.update({'mode': df_mode_value})
-            if df_mode_value == df_modes_supported['pbc'] or df_mode_value == df_modes_supported['dbc']:
-                df_criteria_value.update(df_details['criteria'])    # TODO TEMPORARY approach in case criteria were empty
-                df_params.update({'criteria': df_criteria_value})
+            df_needs_updating = True
+        if isinstance(df_criteria, dict) and len(df_criteria) > 0:
+            df_criteria.update(df_details['criteria'])
+            if df_criteria != df_details['criteria']:
+                print("Updating DF %s criteria" % (df_details['default_name']))
+                df_params.update({'criteria': df_criteria})
+                df_needs_updating = True
+        if df_needs_updating:
             nto.modifyFilter(str(df['id']), df_params)
-            # TODO handle errors
+        # TODO handle errors
     else:
         # This should never happen, but just in case, provide details to look into
         print("Found more than one DF named %s, can't continue:" % (df_name)),
@@ -118,15 +129,17 @@ df_name = ''            # Name for Dynamic Filter to work with
 network_pg_name = ''    # Name for the network port group to connect to the DF
 tool_pg_name = ''       # Name for the tool port group to connect to the DF
 df_mode = ''            # Mode for Dynamic Filter
+criteria_file = ''      # File with dynamic filter criteria in JSON format
+df_criteria = None      # Criteria for Dynamic Filter after pasing criteria_file
 host = ''
 hosts_file = ''
 config_file = ''
 port = 8000
 
-usage = 'ixvision_ztp_filter.py -u <username> -p <password> -n <dynamic_filter_name> -i <network_port_group_name> -o <tool_port_group_name> -m all|none|pbc|dbc|pbcu|dbcm [-h <hosts> | -f <host_file>] [-r port]'
+usage = 'ixvision_ztp_filter.py -u <username> -p <password> -n <dynamic_filter_name> -i <network_port_group_name> -o <tool_port_group_name> -m all|none|pbc|dbc|pbcu|dbcm -c <filter_criteria_file> [-h <hosts> | -f <host_file>] [-r port]'
 
 try:
-    opts, args = getopt.getopt(argv,"u:p:h:f:r:n:i:o:m:", ["username=", "password=", "host=", "hosts_file=", "port=", "name=", "input=", "output=", "mode="])
+    opts, args = getopt.getopt(argv,"u:p:h:f:r:n:i:o:m:c:", ["username=", "password=", "host=", "hosts_file=", "port=", "name=", "input=", "output=", "mode=", "criteria_file="])
 except getopt.GetoptError:
     print usage
     sys.exit(2)
@@ -146,6 +159,8 @@ for opt, arg in opts:
             df_mode = arg
         else:
             print("Unsupported filter mode %s" % (arg))
+    elif opt in ("-c", "--criteria_file"):
+        criteria_file = arg
     elif opt in ("-h", "--host"):
         host = arg
     elif opt in ("-f", "--hosts_file"):
@@ -164,6 +179,26 @@ if (host == '') and (hosts_file == ''):
 if df_name == '' or network_pg_name == '' or tool_pg_name == '' or df_mode == '':
     print usage
     sys.exit(2)
+    
+if df_criteria_required(df_mode) and criteria_file == '':
+    print ("Error: criteria file is requied for dynamic filter mode %s" % (df_mode))
+    sys.exit(2)
+
+if criteria_file != '':
+    parse_failed = False
+    try:
+        with open(criteria_file) as f:
+            try:
+                df_criteria = json.load(f)
+                print("Filter criteria has been loaded")
+            except:
+                print("Error: can't parse filter criteria")
+                parse_failed = True
+                sys.exit(2)
+    except:
+        if not parse_failed:
+            print("Error: can't read the file with filter criteria")
+        sys.exit(2)
 
 hosts_list = []
 if (hosts_file != ''):
@@ -181,7 +216,7 @@ for host in hosts_list:
     host_ip = host[0]
     
     print("DEBUG: Starting thread for %s" % (host_ip))
-    thread = threading.Thread(name=host, target=form_dynamic_filter, args=(host_ip, port, username, password, df_name, network_pg_name, tool_pg_name, df_mode))
+    thread = threading.Thread(name=host, target=form_dynamic_filter, args=(host_ip, port, username, password, df_name, network_pg_name, tool_pg_name, df_mode, df_criteria))
     threads_list.append(thread)
 
 for thread in threads_list:
